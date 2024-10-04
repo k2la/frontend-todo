84196ddbfa07199e3185e74fa4abb0aaf81f82f9
古田悠人 <noon@furutayuuninnoMacBook-Pro.local>
2024-10-04 23:58:13 +0900

    仮の属性を設定

--- app/_components/Todo/index.tsx
+++ app/_components/Todo/index.tsx
@@ -1,3 +1,13 @@
-export default function Todo() {
-  return <>Todo</>;
+type Props = {
+  id: number;
+  content: string;
+};
+
+export default function Todo({ id, content }: Props) {
+  return (
+    <>
+      <p>{id}</p>
+      <p>{content}</p>
+    </>
+  );
 }
--- app/_components/TodoList/index.tsx
+++ app/_components/TodoList/index.tsx
@@ -1,3 +1,22 @@
-export default function TodoList() {
-  return <>TodoList</>;
+import Todo from "@/app/_components/Todo";
+
+type TodoType = {
+  id: number;
+  content: string;
+};
+
+type Props = {
+  todolist: TodoType[];
+};
+
+export default function TodoList({ todolist }: Props) {
+  return (
+    <>
+      {todolist.map((todo) => (
+        <div key={todo.id}>
+          <Todo id={todo.id} content={todo.content} />
+        </div>
+      ))}
+    </>
+  );
 }
