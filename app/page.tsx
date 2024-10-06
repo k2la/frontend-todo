66e4374d8b5ca807980ee4775eb5ef00dd31a8f3
古田悠人 <noon@furutayuuninnoMacBook-Pro.local>
2024-10-06 13:20:50 +0900

    MUIを導入

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,6 @@
 import Title from "@/app/_components/Title";
 import TodoList from "@/app/_components/TodoList";
-
-const todolist = [
-  {
-    id: 1,
-    content: "test1",
-  },
-  {
-    id: 2,
-    content: "test2",
-  },
-  {
-    id: 3,
-    content: "test3",
-  },
-];
+import { todolist } from "./_utils/data";
 
 export default function Home() {
   return (
